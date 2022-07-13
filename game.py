0112630aee872010eb762b31a7fbe3d3b9dd7610
PattonChandler <Patton.Chandler@stern.nyu.edu>
2022-07-12 21:53:25 -0500

    Completed

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import random
-print("Rock, Paper, Scissors, Shoot!")
+print("Welcome Player One to Rock, Paper, Scissors, Shoot!")
 # USER INPUTS
 user_choice = input("Please make a selection ('rock', 'paper', 'scissors'):")
 user_choice = user_choice.lower()
@@ -7,11 +7,28 @@ print(f"You chose: '{user_choice}'")
 # VALIDATE USER INPUTS
 valid_options = ["rock", "paper", "scissors"]
 if (user_choice not in valid_options):
-    print("Not a valid option, please try again")
+    print("Not a valid option, please try again!")
     exit()
 # COMPUTER CHOICE
 computer_choice = random.choice(valid_options)
-print("Computer chose:", computer_choice)
+print("Computer choses:", computer_choice)
 # DETERMINE THE WINNER
-
+if user_choice == computer_choice:
+    print("Tie! Please try again")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print("You lose, sorry, try again!")
+    else:
+        print("You win and all of your dreams will come true!")
+elif user_choice == "rock":
+    if computer_choice == "paper":
+        print("You lose, sorry, try again!")
+    else:
+        print("You win and all of your dreams will come true!")
+elif user_choice == "paper":
+    if computer_choice == "scissors":
+        print("You lose, sorry, try again!")
+    else:
+        print("You win and all of your dreams will come true!")
 # DISPLAY RESULTS
+print("Thank you for playing, if you won, GREAT! If not, it might be time to consider another career path, a professional rock, paper, scissors player is not in the cards for you.")
