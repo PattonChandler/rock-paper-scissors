be2109992fc85952e6f4796797206e998b95649f
PattonChandler <Patton.Chandler@stern.nyu.edu>
2022-07-12 19:31:14 -0500

    Random Computer Choice

---
+++ game.py
@@ -0,0 +1,14 @@
+import random
+print("Rock, Paper, Scissors, Shoot!")
+# USER INPUTS
+user_choice = input("Please make a selection ('rock', 'paper', 'scissors'):")
+print(f"You chose: '{user_choice}'")
+# VALIDATE USER INPUTS
+
+# COMPUTER CHOICE
+valid_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(valid_options)
+print("Computer chose:", computer_choice)
+# DETERMINE THE WINNER
+
+# DISPLAY RESULTS
