069cca0958c2da15bbb8490721f961f69a5c85d1
PattonChandler <Patton.Chandler@stern.nyu.edu>
2022-07-12 21:05:37 -0500

    Validated Choice

--- game.py
+++ game.py
@@ -2,11 +2,14 @@ import random
 print("Rock, Paper, Scissors, Shoot!")
 # USER INPUTS
 user_choice = input("Please make a selection ('rock', 'paper', 'scissors'):")
+user_choice = user_choice.lower()
 print(f"You chose: '{user_choice}'")
 # VALIDATE USER INPUTS
-
-# COMPUTER CHOICE
 valid_options = ["rock", "paper", "scissors"]
+if (user_choice not in valid_options):
+    print("Not a valid option, please try again")
+    exit()
+# COMPUTER CHOICE
 computer_choice = random.choice(valid_options)
 print("Computer chose:", computer_choice)
 # DETERMINE THE WINNER
